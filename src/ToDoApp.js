8f8c09279697fda7f8c9f19c7463e5a27026caa7
Abel Guitron <guitronaca@gmail.com>
2022-01-27 19:43:35 -0600

    :tada: Starting TODO app.

---
+++ src/ToDoApp.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const ToDOApp = () => {
+  const [tasks, setTasks] = useState([]);
+
+  return (
+    <>
+      <h1>ToDoApp</h1>
+    </>
+  );
+};
+
+export default ToDOApp;
